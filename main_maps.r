780f40a6490b1ceaadcf03a65e1b9b6e73e3e0b3
goyanedelv <goyanedelv@gmail.com>
2021-05-12 15:11:06 -0500

    mapas a nivel distritos y otras mejoras

--- main_maps.r
+++ main_maps.r
@@ -14,29 +14,35 @@ Encoding(map$NOM_PROV) <- "UTF-8"
 
 datos = read.xlsx("datos.xlsx")
 
+# 
+
 datos$comunas = as.factor(datos$comunas)
 datos$psupond = as.numeric(datos$psupond)
 
 map@data = data.frame(map@data,datos[match(map@data[,"COD_COMUNA"],datos[,"COD_COMUNA"]),])
 
-pal1 <- colorNumeric("Spectral",NULL)
-pal2 <- colorNumeric("PiYG",NULL)
+pal1 <- colorNumeric("Set3",NULL)
+pal2 <- colorNumeric("Set2",NULL)
 
 m <-leaflet(map) %>%
-	addTiles() %>%
+  addEasyButton(easyButton(
+    icon="fa-crosshairs", title="Ir a mi ubicación",
+    onClick=JS("function(btn, map){ map.locate({setView: true}); }")))%>%
+    addTiles() %>%
+    setView(-70.657,-33.478,5) %>% 
 	addProviderTiles(providers$OpenStreetMap) %>%
 	addPolygons(stroke = FALSE, 
                 smoothFactor = 0.3, 
                 fillOpacity = 0.8,
-    	        fillColor = ~pal1(rnorm(346,0,1)),
-    	        label = ~paste0(map$NOM_COM, ": ", formatC(map$psupond, big.mark = ",")),
+    	        fillColor = ~pal1(map$distrito),
+    	        label = ~paste0("Distrito ", formatC(map$distrito, big.mark = ","), ": Data de prueba 1"),
                 group = 'Vamos por Chile') %>%
 
 	addPolygons(stroke = FALSE, 
                 smoothFactor = 0.3, 
                 fillOpacity = 0.8,
-    	        fillColor = ~pal2(rnorm(346,0,1)),
-    	        label = ~paste0(map$NOM_COM, ": ", formatC(map$psupond, big.mark = ",")),
+    	        fillColor = ~pal2(map$distrito),
+    	        label = ~paste0("Distrito : ", formatC(map$distrito, big.mark = ","), ": Data de prueba 2"),
                 group = 'Lista del Apruebo') %>%
     addLayersControl(c("Vamos por Chile", "Lista del Apruebo"), 
         options = layersControlOptions(collapsed = FALSE))
@@ -44,4 +50,6 @@ m <-leaflet(map) %>%
 m
 
 saveWidget(m, file="index.html") 
+
+
 
